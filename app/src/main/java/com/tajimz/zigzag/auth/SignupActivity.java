4486d2c5744c9a46695bc8be9e8696cf0329e66d
tajimZ <com.tajim@gmail.com>
2025-10-24 15:11:34 +0600

    done

--- app/src/main/java/com/tajimz/zigzag/auth/SignupActivity.java
+++ app/src/main/java/com/tajimz/zigzag/auth/SignupActivity.java
@@ -1,18 +1,37 @@
 package com.tajimz.zigzag.auth;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.tajimz.zigzag.R;
 import com.tajimz.zigzag.databinding.ActivitySignupBinding;
 import com.tajimz.zigzag.helper.BaseActivity;
 import com.tajimz.zigzag.helper.CONSTANTS;
 
+import androidx.credentials.CredentialManager;
+import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption;
+import androidx.credentials.CredentialManagerCallback;
+import androidx.credentials.GetCredentialRequest;
+import androidx.credentials.GetCredentialResponse;
+import androidx.credentials.Credential;
+import androidx.credentials.exceptions.GetCredentialException;
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignupActivity extends BaseActivity {
     private ActivitySignupBinding binding;
     private String name, email, pass, conPass;
+
+    FirebaseAuth firebaseAuth;
+    CredentialManager credentialManager;
+    GetSignInWithGoogleOption googleOption;
+    GetCredentialRequest getCredentialRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +39,9 @@ public class SignupActivity extends BaseActivity {
         binding = ActivitySignupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setupEdge();
+        handleGoogle();
+
+
 
 
 
@@ -32,6 +54,10 @@ public class SignupActivity extends BaseActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        firebaseAuth = FirebaseAuth.getInstance();
+        credentialManager = CredentialManager.create(this);
+        googleOption = new GetSignInWithGoogleOption.Builder(getString(R.string.googleId)).build();
+        getCredentialRequest = new GetCredentialRequest.Builder().addCredentialOption(googleOption).build();
 
     }
 
@@ -86,6 +112,27 @@ public class SignupActivity extends BaseActivity {
 
 
 
+    }
+
+    private void handleGoogle(){
+        binding.tvVerify.setOnClickListener(v->{
+            credentialManager.getCredentialAsync(this, getCredentialRequest, null, Runnable::run, new CredentialManagerCallback<GetCredentialResponse, GetCredentialException>() {
+                @Override
+                public void onResult(GetCredentialResponse getCredentialResponse) {
+
+                    Credential credential = getCredentialResponse.getCredential();
+                    GoogleIdTokenCredential googleIdTokenCredential = GoogleIdTokenCredential.createFrom(credential.getData());
+                    Log.d("tustus","heree");
+
+
+                }
+
+                @Override
+                public void onError(@NonNull GetCredentialException e) {
+                    Log.d("tustus",e.getMessage().toString());
+                }
+            });
+        });
     }
 
 
